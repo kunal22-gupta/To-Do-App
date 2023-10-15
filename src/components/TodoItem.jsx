232fba3aa9f555c116637a8022d8880997287f7d
Kunal Gupta <kunal22.10.2002@gmail.com>
2023-10-15 07:16:09 +0530

    create TodoItem component for list items in todos

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,34 +1,57 @@
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import Checkbox from "@mui/material/Checkbox";
-import IconButton from "@mui/material/IconButton";
-import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
-export default function TodoItem({item, removeTodo, toggleCheck}) {
-    const labelId = `checkbox-list-label-${item.id}`
+import { useState } from "react";
+import { useTodo } from "../context/TodoContext";
+
+export default function TodoItem({ todo }) {
+    const [isEditable, setIsEditable] = useState(false);
+    const [todoText, setTodoText] = useState(todo.todo);
+
+    const { editTodo, toggleComplete, deleteTodo } = useTodo();
+
+    const handleEdit = () => {
+        editTodo(todo.id, { ...todo, todo: todoText });
+        setIsEditable(false);
+    };
+    const handleToggle = () => {
+        toggleComplete(todo.id);
+    };
+
     return (
-        <ListItem
-          secondaryAction={
-            <IconButton edge="end" aria-label="comments" onClick={() => removeTodo(item.id)}>
-              <CloseRoundedIcon />
-            </IconButton>
-          }
-          disablePadding
-        >
-          <ListItemButton role={undefined} dense>
-            <ListItemIcon>
-              <Checkbox
-                edge="start"
-                checked={item.completed}
-                tabIndex={-1}
-                disableRipple
-                inputProps={{ 'aria-labelledby': labelId }}
-                onChange={() => toggleCheck(item.id)}
-              />
-            </ListItemIcon>
-            <ListItemText id={labelId} primary={item.text} />
-          </ListItemButton>
-        </ListItem>
-      );
+        <div className="TodoItem">
+            <div className="container">
+                <form onSubmit={(e) => e.preventDefault()}>
+                <input
+                    type="checkbox"
+                    name="complete"
+                    id="complete"
+                    checked={todo.isCompleted}
+                    onChange={handleToggle}
+                />
+                <input
+                    type="text"
+                    name="todo"
+                    id="todo"
+                    value={todoText}
+                    readOnly={!isEditable}
+                    onChange={(e) => setTodoText(e.target.value)}
+                />
+                <button
+                    disabled={todo.isCompleted}
+                    onClick={() => {
+                        if (todo.isCompleted) return;
+                        if (isEditable) {
+                            handleEdit();
+                        } else {
+                            setIsEditable((currVal) => !currVal);
+                        }
+                    }}
+                >
+                    <i className="ri-pencil-fill"></i>
+                </button>
+                <button onClick={() => deleteTodo(todo.id)}>
+                    <i className="ri-delete-bin-6-fill"></i>
+                </button>
+                </form>
+            </div>
+        </div>
+    );
 }
