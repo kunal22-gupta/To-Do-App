87e869d560230bf0d5a3c67a75ffc0cf10139921
Kunal Gupta <kunal22.10.2002@gmail.com>
2023-10-15 05:47:06 +0530

    update App.jsx to use context and add local storage for todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,66 @@
-import CssBaseline from '@mui/material/CssBaseline'
-
-import TodoList from './TodoList'
-import './App.css'
+import "./App.css";
+import { useEffect, useState } from "react";
+import { TodoProvider } from "./context/index";
+import TodoForm from "./components/TodoForm";
 
 function App() {
+    // Store the todos list data
+    const [todos, setTodos] = useState([]);
 
-  return (
-    <>
-      <CssBaseline/>
-      <TodoList/>
-    </>
-  )
+    // add a new todo item to the list
+    const addTodo = (todo) => {
+        setTodos((currTodos) => {
+            return [{ id: Date.now(), todo }, ...currTodos];
+        });
+    };
+    // edit the current todo item in the list
+    const editTodo = (id, todo) => {
+        setTodos((currTodos) => {
+            currTodos.map((currTodo) => {
+                return currTodo.id === id ? { ...todo } : { ...currTodo };
+            });
+        });
+    };
+    // delete a specific todo item in the list
+    const deleteTodo = (id) => {
+        setTodos((currTodos) => {
+            currTodos.filter((currTodo) => {
+                return !(currTodo.id === id);
+            });
+        });
+    };
+    // toggle between complete and incomplete
+    const toggleComplete = (id) => {
+        setTodos((currTodos) => {
+            currTodos.map((currTodo) => {
+                return currTodo.id === id
+                    ? { ...currTodo, isCompleted: !currTodo.isCompleted }
+                    : currTodo;
+            });
+        });
+    };
+
+    // Add local storage to preserve todo list on page reload/refresh
+    useEffect(() => {
+        // for the first render
+        const todos = JSON.parse(localStorage.getItem("todos"));
+        if (todos && todos.length > 0) {
+            setTodos(todos);
+        }
+    }, []);
+
+    useEffect(() => {
+        // for every todos state re-render
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
+
+    return (
+        <TodoProvider
+            value={{ todos, addTodo, editTodo, deleteTodo, toggleComplete }}
+        >
+            <TodoForm />
+        </TodoProvider>
+    );
 }
 
-export default App
+export default App;
