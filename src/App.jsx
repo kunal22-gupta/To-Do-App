4840a30f37eaec87c54e59e3066ab1f829180676
Kunal Gupta <kunal22.10.2002@gmail.com>
2023-10-15 07:14:25 +0530

    fix: add return when calling setTodos

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import { TodoProvider } from "./context/index";
 import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem";
 
 function App() {
     // Store the todos list data
@@ -9,30 +10,30 @@ function App() {
 
     // add a new todo item to the list
     const addTodo = (todo) => {
-        setTodos((currTodos) => {
-            return [{ id: Date.now(), todo }, ...currTodos];
+      setTodos((currTodos) => {
+            return [{ id: Date.now(), ...todo }, ...currTodos];
         });
     };
     // edit the current todo item in the list
     const editTodo = (id, todo) => {
         setTodos((currTodos) => {
-            currTodos.map((currTodo) => {
-                return currTodo.id === id ? { ...todo } : { ...currTodo };
+            return currTodos.map((currTodo) => {
+                return currTodo.id === id ? todo : currTodo;
             });
         });
     };
     // delete a specific todo item in the list
     const deleteTodo = (id) => {
         setTodos((currTodos) => {
-            currTodos.filter((currTodo) => {
-                return !(currTodo.id === id);
+            return currTodos.filter((currTodo) => {
+                return (currTodo.id !== id);
             });
         });
     };
     // toggle between complete and incomplete
     const toggleComplete = (id) => {
         setTodos((currTodos) => {
-            currTodos.map((currTodo) => {
+            return currTodos.map((currTodo) => {
                 return currTodo.id === id
                     ? { ...currTodo, isCompleted: !currTodo.isCompleted }
                     : currTodo;
@@ -59,6 +60,12 @@ function App() {
             value={{ todos, addTodo, editTodo, deleteTodo, toggleComplete }}
         >
             <TodoForm />
+            {
+            todos.map((todo) => (
+                <div key={todo.id}>
+                    <TodoItem todo={todo}/>
+                </div>
+            ))}
         </TodoProvider>
     );
 }
