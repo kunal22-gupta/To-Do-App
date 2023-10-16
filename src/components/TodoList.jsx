0d5586f35f5ba729e18d556ab0ade5e45be6e86b
Kunal Gupta <kunal22.10.2002@gmail.com>
2023-10-16 23:11:31 +0530

    render TodoItems in TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,88 +1,13 @@
-import List from "@mui/material/List";
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
-
-import { v4 as uuid } from "uuid";
+import "../css/TodoList.css";
 import TodoItem from "./TodoItem";
-import TodoForm from "./TodoForm";
-
-import { useState, useEffect } from "react";
-
-const initialTodos = JSON.parse(localStorage.getItem("todos")) || [];
-
+import { useTodo } from "../context";
 export default function TodoList() {
-    const [todos, setTodos] = useState(initialTodos);
-    const [newTodo, setNewTodo] = useState({
-        id: uuid(),
-        text: "",
-        completed: false,
-    });
-
-    // Save todos in my LocalStorage
-    useEffect(() => {
-        localStorage.setItem("todos", JSON.stringify(todos));
-    }, [todos]);
-
-    // Remove an item from todo list
-    const removeTodo = (id) => {
-        setTodos((currTodos) => {
-            return currTodos.filter((todo) => todo.id !== id);
-        });
-    };
-
-    // Toggle when item is checked and unchecked
-    const toggleCheck = (id) => {
-        setTodos((currTodos) => {
-            return currTodos.map((todo) => {
-                if (id === todo.id) {
-                    return { ...todo, completed: !todo.completed };
-                } else {
-                    return todo;
-                }
-            });
-        });
-    };
-
-    // Sync the input field
-    const handleChange = (e) => {
-        setNewTodo((currItem) => {
-            return { ...currItem, text: e.target.value };
-        });
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setTodos((currTodos) => {
-            return [...currTodos, newTodo];
-        });
-        setNewTodo({ id: uuid(), text: "", completed: false });
-    };
+    const { todos } = useTodo();
     return (
-        <Container maxWidth="sm">
-            <h1>Todo List</h1>
-            <Box>
-                <List
-                    sx={{
-                        width: "100%",
-                        maxWidth: 360,
-                        bgcolor: "background.paper",
-                    }}
-                >
-                    <TodoForm
-                        text={newTodo.text}
-                        handleChange={handleChange}
-                        handleSubmit={handleSubmit}
-                    />
-                    {todos.map((item) => (
-                        <TodoItem
-                            key={item.id}
-                            item={item}
-                            removeTodo={removeTodo}
-                            toggleCheck={toggleCheck}
-                        />
-                    ))}
-                </List>
-            </Box>
-        </Container>
+        <div className="TodoList">
+            {todos.map((todo) => (
+                <TodoItem todo={todo} key={todo.id}/>
+            ))}
+        </div>
     );
 }
