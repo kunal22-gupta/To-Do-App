a4224d06a4540a997870f9035fa5e18e61a1d375
Kunal Gupta <kunal22.10.2002@gmail.com>
2023-10-16 06:36:32 +0530

    import css and wrap form in a wrapper div

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,29 +1,32 @@
 import { useTodo } from "../context/index.js";
 import { useState } from "react";
+import "../css/TodoForm.css";
 
 export default function TodoForm() {
     const [todo, setTodo] = useState("");
 
-    const {addTodo} = useTodo();
+    const { addTodo } = useTodo();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if(!todo) return
-        addTodo({todo: todo, isCompleted: false})
-    }
+        if (!todo) return;
+        addTodo({ todo: todo, isCompleted: false });
+    };
 
     return (
         <form onSubmit={handleSubmit} className="TodoForm">
             <div className="container">
-                <input
-                    type="text"
-                    name="todo"
-                    id="todo"
-                    value={todo}
-                    placeholder="Add Todo..."
-                    onChange={(e) => setTodo(e.target.value)}
-                />
-                <button><i className="ri-add-circle-line"></i></button>
+                <div className="wrapper">
+                    <input
+                        type="text"
+                        name="todo"
+                        id="todo"
+                        value={todo}
+                        placeholder="Add Todo..."
+                        onChange={(e) => setTodo(e.target.value)}
+                    />
+                    <button>ADD</button>
+                </div>
             </div>
         </form>
     );
